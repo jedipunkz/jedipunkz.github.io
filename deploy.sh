c9acd17f627e07522102a0b742ee7e24c5137c51
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2016-06-19 08:42:06 +0900

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+hugo -t hugo-future-imperfect
+
+git add .
+
+if [ $# -eq 1 ] then
+    msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin source
+git subtree push --prefix=public origin master
